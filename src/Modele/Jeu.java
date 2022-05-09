64bf482b1e82cccaf5455b467a5f4b6eee01a10f
Yimi-wang <wmt200802@aliyun.com>
2022-05-09 19:27:04 +0200

    发牌实现(以修改完bug，但超级难看。）

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -1,13 +1,20 @@
 package Modele;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Jeu {
-    static List<Brand>[] playercard=new List[2];//玩家手牌//建立了一个tableau 的list playercard[0]是玩家1的手卡，etc
-    static List<Brand>[] pilescard=new List[6];//牌堆//建立了一个tableau 的list pilescard[0]是第一个牌堆，etc
+    static List<Brand>[] playercard;//=new List[2];//玩家手牌//建立了一个tableau 的list playercard[0]是玩家1的手卡，etc
+    static List<Brand>[] pilescard;//=new List[6];//牌堆//建立了一个tableau 的list pilescard[0]是第一个牌堆，etc
     public int numberOfGames=0;//回合数
     public int numberOfRounds;//轮数
     int playerfirst=2 ;//哪个玩家先手，0代表玩家A。1代表玩家B
-    List<Brand> test;
-
+    List<Brand> playcard0=new ArrayList<>();
+    List<Brand> playcard1=new ArrayList<>();
+    List<Brand> pilescard1=new ArrayList<>();
+    List<Brand> pilescard2=new ArrayList<>();
+    List<Brand> pilescard3=new ArrayList<>();
+    List<Brand> pilescard4=new ArrayList<>();
+    List<Brand> pilescard5=new ArrayList<>();
+    List<Brand> pilescard6=new ArrayList<>();
 }
--- src/Modele/Starthand.java
+++ src/Modele/Starthand.java
@@ -18,86 +18,108 @@ public class Starthand {
         Brand card = null;
         createPlayingCard(listcard);//
         //j.playercard=new List[2];//手牌
-       // j.pilescard=new List[6];//牌堆
-        j.test = new ArrayList<>();
+        //j.pilescard=new List[6];//牌堆
         for(int i =0; i <52; i++){
             int coleur=(listcard.get(i))/13;
             switch (coleur){
                 case 0:
-                    card = new Brand(Type.pique,new Integer((listcard.get(i))%13));
+                    card = new Brand(Type.pique, (listcard.get(i)) % 13);
                     break;
                 case 1:
-                    card = new Brand(Type.coeur,new Integer((listcard.get(i))%13));
+                    card = new Brand(Type.coeur, (listcard.get(i)) % 13);
                     break;
                 case 2:
-                    card = new Brand(Type.carreau,new Integer((listcard.get(i))%13));
+                    card = new Brand(Type.carreau, (listcard.get(i)) % 13);
                     break;
                 case 3:
-                    card = new Brand(Type.trefle,new Integer((listcard.get(i))%13));
+                    card = new Brand(Type.trefle, (listcard.get(i)) % 13);
                     break;
             }
         Brand c=card;
         if(i<11){
-            j.test.add(c);
+            j.playcard0.add(c);
           //  (j.playercard[0]).add(c);
         }
-        else if(i>10 && i<21){
-            j.playercard[1].add(card);
+        else if(i>10 && i<22){
+            //j.playercard[1].add(card);
+            j.playcard1.add(c);
         }
         else{
-            j.pilescard[(i-21)%6].add(card);
+            //j.pilescard[(i-21)%6].add(card);
+            switch ((i-21)%6){
+                case 0:
+                    j.pilescard1.add(c);
+                    break;
+                case 1:
+                    j.pilescard2.add(c);
+                    break;
+                case 2:
+                    j.pilescard3.add(c);
+                    break;
+                case 3:
+                    j.pilescard4.add(c);
+                    break;
+                case 4:
+                    j.pilescard5.add(c);
+                    break;
+                case 5:
+                    j.pilescard6.add(c);
+                    break;
+
+            }
+
         }
         }
         showcard(j);
     }
 
     private static void showcard(Jeu j) {
+        System.out.println("player 1 card");
         for(int i=0;i<11;i++){
-            System.out.println("player 1 card");
             //Brand b =  j.playercard[0].get(i);
-            Brand b = j.test.get(i);
+            Brand b = j.playcard0.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("player 2 card");
         for(int i=0;i<11;i++){
-            System.out.println("player 2 card");
-            Brand b =  j.playercard[1].get(i);
+            Brand b =  j.playcard1.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("pile 1 card");
         for(int i=0;i<5;i++){
-            System.out.println("pile card");
-            Brand b =  j.pilescard[0].get(i);
+            Brand b =  j.pilescard1.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("pile 2 card");
         for(int i=0;i<5;i++){
-            System.out.println("pile card");
-            Brand b =  j.pilescard[1].get(i);
+            Brand b =  j.pilescard2.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("pile 3 card");
         for(int i=0;i<5;i++){
-            System.out.println("pile card");
-            Brand b =  j.pilescard[2].get(i);
+            Brand b =  j.pilescard3.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("pile 4 card");
         for(int i=0;i<5;i++){
-            System.out.println("pile card");
-            Brand b =  j.pilescard[3].get(i);
+            Brand b =  j.pilescard4.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("pile 5 card");
         for(int i=0;i<5;i++){
-            System.out.println("pile card");
-            Brand b =  j.pilescard[4].get(i);
+            Brand b =  j.pilescard5.get(i);
             String s = b.toString();
             System.out.println(s);
         }
+        System.out.println("pile 6 card");
         for(int i=0;i<5;i++){
-            System.out.println("pile card");
-            Brand b =  j.pilescard[5].get(i);
+            Brand b =  j.pilescard6.get(i);
             String s = b.toString();
             System.out.println(s);
         }
